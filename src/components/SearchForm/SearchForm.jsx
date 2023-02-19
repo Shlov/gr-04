0cb839a5fe5d44299f9e2a121893e4f7e2aafc05
Anton Shabalov <shabalovanton@gmail.com>
2023-02-19 11:36:51 +0200

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
